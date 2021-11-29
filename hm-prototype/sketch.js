bcbecaf7b9a0ee25af105f05e7f2084f5a055f17
marshall-holly <79159730+marshall-holly@users.noreply.github.com>
2021-11-29 18:02:34 -0500

    Add files via upload

---
+++ hm-prototype/sketch.js
@@ -0,0 +1,80 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
+let video;
+let poseNet;
+let poses = [];
+let img1;
+let img2;
+
+function preload() {
+    img1 = loadImage('images/saturn.png');
+    img2 = loadImage('images/rocket.png');
+}
+
+function setup() {
+    createCanvas(1280, 960);
+    video = createCapture(VIDEO);
+    video.size(width, height);
+
+    // Create a new poseNet method with a single detection
+    poseNet = ml5.poseNet(video, {
+        outputStride: 8,
+        quantBytes: 4
+    }, modelReady);
+    // This sets up an event that fills the global variable "poses"
+    // with an array every time new poses are detected
+    poseNet.on('pose', function (results) {
+        poses = results;
+    });
+    // Hide the video element, and just show the canvas
+    video.hide();
+}
+
+function modelReady() {
+    select('#status').html('Model Loaded');
+}
+
+function mousePressed() {
+    console.log(JSON.stringify(poses))
+}
+
+function draw() {
+    image(video, 0, 0, width, height);
+    strokeWeight(2);
+
+    // For one pose only (use a for loop for multiple poses!)
+    if (poses.length > 0) {
+        const pose = poses[0].pose;
+        console.log(pose);
+
+        // Create a orange rectangle for the carrot nose
+        fill(0);
+        const nose = pose.nose;
+        image(img2, nose.x, nose.y, 150, 150);
+
+        const rightEye = pose.rightEye;
+        image(img1, rightEye.x, rightEye.y - 200, 100, 100);
+
+        fill(0);
+        const leftEye = pose.leftEye;
+        ellipse(leftEye.x, leftEye.y, 50, 50);
+
+
+        // Create a yellow ellipse for the right eye
+        //fill(255, 215, 0);
+        //const leftEye = pose.leftEye;
+        //ellipse(leftEye.x, leftEye.y, 20, 20);
+
+        //fill(0, 255, 0);
+        //const rightShoulder = pose.rightShoulder;
+        //ellipse(rightShoulder.x, rightShoulder.y, 20, 20);
+    }
+}
